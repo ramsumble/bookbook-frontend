2436624b0db5cb8cb3d0903de4516b86f367f6d3
ramsumble <ram.sumble@gmail.com>
2024-02-13 00:15:58 +1100

    first commit! interacting with the express book search route

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const BookSearch = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  // Function to handle the search button click
+  const handleSearch = async () => {
+    try {
+      // console.log(process.env.REACT_APP_BACKEND_URL)
+      const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}`, {
+        params: { searchTerm },
+      });
+
+      // Update the component state with the search results
+      setSearchResults(response.data);
+    } catch (error) {
+      console.error('Error searching books:', error);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+
+      {/* Display the search results in a UL */}
+      <ul>
+        {searchResults.map((book) => (
+          <li key={book._id}>{book.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default BookSearch;
