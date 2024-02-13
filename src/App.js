6470da1e2e403057156f45fb717f98970da4519c
ramsumble <ram.sumble@gmail.com>
2024-02-13 13:20:49 +1100

    separated booksearch function into its own file

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import BookSearch from './pages/Booksearch'; 
 
-const BookSearch = () => {
-
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-
-  // Function to handle the search button click
-  const handleSearch = async () => {
-    try {
-      // console.log(process.env.REACT_APP_BACKEND_URL)
-      const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}`, {
-        params: { searchTerm },
-      });
-
-      // Update the component state with the search results
-      setSearchResults(response.data);
-    } catch (error) {
-      console.error('Error searching books:', error);
-    }
-  };
-
+const App = () => {
   return (
     <div>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <button onClick={handleSearch}>Search</button>
-
-      {/* Display the search results in a UL */}
-      <ul>
-        {searchResults.map((book) => (
-          <li key={book._id}>{book.title}</li>
-        ))}
-      </ul>
+      <BookSearch />
     </div>
   );
 };
 
-export default BookSearch;
+export default App;
