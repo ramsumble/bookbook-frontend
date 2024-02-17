bd9f7cd67fcb1c1fae6c26f694175157f9f77234
ramsumble <ram.sumble@gmail.com>
2024-02-17 22:48:25 +1100

    separated quotes into a component, and the array of quotes into utils

---
+++ src/components/Quotes.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import quotes from '../utils/quotes';
+
+const RandomQuoteComponent = () => {
+    const [randomQuote, setRandomQuote] = useState(() => {
+        const selectedQuote = quotes[Math.floor(Math.random() * quotes.length)];
+        return selectedQuote;
+    });
+
+    return (
+        <div>
+            <h2>{randomQuote.quote}</h2>
+            <h3>- {randomQuote.author}</h3>
+        </div>
+    );
+};
+
+export default RandomQuoteComponent;
---
+++ src/utils/quotes.js
@@ -0,0 +1,61 @@
+
+const quotes = [
+    {
+      quote: "So many books, so little time.",
+      author: "Frank Zappa"
+    },
+    {
+      quote: "A room without books is like a body without a soul.",
+      author: "Marcus Tullius Cicero"
+    },
+    {
+      quote: "The person, be it gentleman or lady, who has not pleasure in a good novel, must be intolerably stupid.",
+      author: "Jane Austen, Northanger Abbey"
+    },
+    {
+      quote: "Good friends, good books, and a sleepy conscience: this is the ideal life.",
+      author: "Mark Twain"
+    },
+    {
+      quote: "If you only read the books that everyone else is reading, you can only think what everyone else is thinking.",
+      author: "Haruki Murakami, Norwegian Wood"
+    },
+    {
+      quote: "I have always imagined that Paradise will be a kind of library.",
+      author: "Jorge Luis Borges"
+    },
+    {
+      quote: "Never trust anyone who has not brought a book with them.",
+      author: "Lemony Snicket, Horseradish: Bitter Truths You Can't Avoid"
+    },
+    {
+      quote: "You can never get a cup of tea large enough or a book long enough to suit me.",
+      author: "C.S. Lewis"
+    },
+    {
+      quote: "If one cannot enjoy reading a book over and over again, there is no use in reading it at all.",
+      author: "Oscar Wilde"
+    },
+    {
+      quote: "There is no friend as loyal as a book.",
+      author: "Ernest Hemingway"
+    },
+    {
+      quote: "′Classic′ - a book which people praise and don't read.",
+      author: "Mark Twain"
+    },
+    {
+      quote: "Books are a uniquely portable magic.",
+      author: "Stephen King, On Writing: A Memoir of the Craft"
+    },
+    {
+      quote: "... a mind needs books as a sword needs a whetstone, if it is to keep its edge.",
+      author: "George R.R. Martin, A Game of Thrones"
+    },
+    {
+      quote: "If one cannot enjoy reading a book over and over again, there is no use in reading it at all.",
+      author: "Oscar Wilde"
+    }
+  ]
+
+export default quotes
